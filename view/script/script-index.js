51a5b70e2dd1e2cf1a220e04628f38ddb37d8d10
Benjamin de Jesus Perez Aguilar <benjaminperez201831504@cunoc.edu.gt>
2021-12-09 14:37:48 -0600

    fue terminado los requisitos de mostrar en consola la informacion ingresada

--- view/script/script-index.js
+++ view/script/script-index.js
@@ -1,8 +1,26 @@
 const switchers = [...document.querySelectorAll('.switcher')]
-
 switchers.forEach(item => {
 	item.addEventListener('click', function() {
 		switchers.forEach(item => item.parentElement.classList.remove('is-active'))
 		this.parentElement.classList.add('is-active')
 	})
 })
+function ingresarCliente(){
+	console.log(document.getElementById("ingrese-Nombre-cliente").value);
+	console.log(document.getElementById("ingrese-Apellido").value);
+	console.log(document.getElementById("ingrese-CUI").value);
+	console.log(document.getElementById("ingrese-Departamento").value);
+}
+function registrar(){
+	console.log(document.getElementById("signup-email").value);
+	console.log(document.getElementById("signup-Nombre").value);
+	console.log(document.getElementById("signup-password").value);
+	console.log(document.getElementById("signup-password-confirm").value);
+	if (!((document.getElementById("signup-password").value) === (document.getElementById("signup-password-confirm").value))) {
+		alert("las contraseña no son iguales");
+	}
+}
+function login(){
+	console.log(document.getElementById("login-email").value);
+	console.log(document.getElementById("login-password").value);
+}
